3b5898f37339cbf56974a078744c577c56e9652f
Martin J White <mwhite@berkeley.edu>
2020-04-21 14:13:17 -0700

    Build pars explicitly.

--- lpt_examples.py
+++ lpt_examples.py
@@ -40,7 +40,10 @@ if __name__=="__main__":
     #    alpha0,alpha2,alpha4: counterterms
     #    sn,s0: stochastic contributions to P(k) and sigma^2.
     z,f    = 0.80,0.8076
-    pars   = [0.7, 0.5, -0.3, 0.0, 10, 20, -60, 1800, -1000]
+    biases = [0.70,0.5,-0.3,0.0]
+    cterms = [10.0,20.,-60.]
+    stoch  = [1800.,-1000.]
+    pars   = biases + cterms + stoch
     # Compute the wedges, here we'll just to a single mu.
     mu     = 0.5
     kw,pkw = mome.compute_redshift_space_power_at_mu(pars,f,mu,reduced=True)
