9550ae11e5beaa55c1a7170d834e7193c8eeec3d
sfschen <shifan_chen@berkeley.edu>
2020-05-21 14:04:24 -0700

    Commented out warnings in loginterp.

--- Utils/loginterp.py
+++ Utils/loginterp.py
@@ -4,12 +4,14 @@ from scipy.misc import derivative
 import inspect
 
 def loginterp(x, y, yint = None, side = "both", lorder = 9, rorder = 9, lp = 1, rp = -1,
-              ldx = 1e-6, rdx = 1e-6):
+              ldx = 1e-6, rdx = 1e-6, verbose=False):
     '''
     Extrapolate function by evaluating a log-index of left & right side.
     
     From Chirag Modi's CLEFT code at
     https://github.com/modichirag/CLEFT/blob/master/qfuncpool.py
+    
+    The warning for divergent power laws on both ends is turned off. To turn back on uncomment lines 26-33.
     '''
     
     if yint is None:
@@ -20,12 +22,15 @@ def loginterp(x, y, yint = None, side = "both", lorder = 9, rorder = 9, lp = 1, 
     r =rp
     lneff = derivative(yint, x[l], dx = x[l]*ldx, order = lorder)*x[l]/y[l]
     rneff = derivative(yint, x[r], dx = x[r]*rdx, order = rorder)*x[r]/y[r]
-    if lneff < 0:
-        print( 'In function - ', inspect.getouterframes( inspect.currentframe() )[2][3])
-        print('WARNING: Runaway index on left side, bad interpolation. Left index = %0.3e at %0.3e'%(lneff, x[l]))
-    if rneff > 0:
-        print( 'In function - ', inspect.getouterframes( inspect.currentframe() )[2][3])
-        print('WARNING: Runaway index on right side, bad interpolation. Reft index = %0.3e at %0.3e'%(rneff, x[r]))
+    
+    # uncomment if you like warnings.
+    #if verbose:
+    #    if lneff < 0:
+    #        print( 'In function - ', inspect.getouterframes( inspect.currentframe() )[2][3])
+    #        print('WARNING: Runaway index on left side, bad interpolation. Left index = %0.3e at %0.3e'%(lneff, x[l]))
+    #    if rneff > 0:
+    #        print( 'In function - ', inspect.getouterframes( inspect.currentframe() )[2][3])
+    #        print('WARNING: Runaway index on right side, bad interpolation. Reft index = %0.3e at %0.3e'%(rneff, x[r]))
 
     xl = np.logspace(-12, np.log10(x[l]), 10**5)
     xr = np.logspace(np.log10(x[r]), 12., 10**5)
