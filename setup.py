34944b28ab87cf26e4d507df560b51a9bcc7e97c
Martin J White <mwhite@berkeley.edu>
2020-12-14 13:47:08 -0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="velocileptors-sfschen",
+    version="1.0",
+    author="Stephen Chen",
+    author_email="shifan_chen@berkeley.edu",
+    description="Code for cosmological perturbation theory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sfschen/velocileptors",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
