75c3570b584a80630218c888966e0f47ffcac54b
Martin White <martinjameswhite@users.noreply.github.com>
2024-06-02 06:14:11 -0700

    Update path.

--- velocileptors/Utils/pnw_dst.py
+++ velocileptors/Utils/pnw_dst.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .loginterp import loginterp
+from velocileptors.Utils.loginterp import loginterp
 
 from scipy.interpolate import interp1d
 from scipy.ndimage import gaussian_filter
